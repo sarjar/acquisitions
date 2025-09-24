a9cc1f4acc85ce9b5d00f9ddcf8a1fa1236ada3d
Sarunas Jaraminas <jaraminas.sarunas@gmail.com>
2025-09-24 22:54:28 +0300

    set up security

---
+++ src/config/arcjet.js
@@ -0,0 +1,36 @@
+import arcjet, {
+  shield,
+  detectBot,
+  tokenBucket,
+  slidingWindow,
+} from '@arcjet/node';
+
+const aj = arcjet({
+  // Get your site key from https://app.arcjet.com and set it as an environment
+  // variable rather than hard coding.
+  key: process.env.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: 'LIVE' }),
+    // Create a bot detection rule
+    detectBot({
+      mode: 'LIVE', // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        'CATEGORY:SEARCH_ENGINE', // Google, Bing, etc
+        'CATEGORY:PREVIEW', // Link previews e.g. Slack, Discord
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    slidingWindow({
+      mode: 'LIVE', // Blocks requests. Use "DRY_RUN" to log only
+      interval: '2s', // Time period in seconds
+      max: 5, // Max requests allowed in the interval
+    }),
+  ],
+});
+
+export default aj;
